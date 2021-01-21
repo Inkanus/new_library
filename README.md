a8bf36c43a108fb8b822e0c29e63d25716e164e3
Inkanus <73430904+Inkanus@users.noreply.github.com>
2021-01-21 16:28:01 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,115 @@
+# Home Library
+
+Home Library is a flask-based website that lets you save your favorite books.
+
+## Prerequisites
+
+* Python 3.9+
+
+## Installation
+
+Clone the repo via Git
+```
+git clone https://github.com/..
+```
+Install packages required using pip
+```
+pip install -r requirements.txt
+```
+
+## Usage
+
+Run the server
+```
+set FLASK_APP=home_library.py
+python -m flask run
+```
+Visit http://localhost:5000 using your browser, from there start adding books
+using the dedicated form or view them in the My Books section, sorting as you prefer.
+You can take a closer look to each book by clicking on their titles,
+and also edit their labels or delete them completely.
+
+## REST Api
+
+Home Library is provided with a REST Api to get, create, edit and delete books in your
+library using HTTP requests.
+
+All of the examples provided are using python requests library.
+
+### GET
+
+Use GET requests to get all of the library data or a specified book's information.
+
+```python
+import requests
+
+# Getting the full library
+requests.get("http://localhost:5000/api/v1/books")
+
+# Specify url parameters to get a sorted copy of library
+params = {
+    "sort_key": "title", # or "author", "year", "pages"
+    "sort_order": "asc" # or "desc"}
+# Passing them to the call to requests.get
+requests.get("http://localhost:5000/api/v1/books", params=params)
+
+# Getting data from a specific book
+book_id = 0
+requests.get("http://localhost:5000/api/v1/books/%d" % book_id)
+```
+
+### POST
+
+Use POST requests to save new books into the library, send json data.
+
+```python
+import json
+import requests
+
+# "genres" and "description" are optional and can be not specified
+data = {
+    "title": "Eragon",
+    "author": [
+        "Christopher Paolini",  # specify up to three genres, leave empty strings
+        "", ""
+    ]
+    "year": 2002, 
+    "genres": [
+        "fantasy", "dystopian", "" # specify up to three genres, leave empty strings
+    ],
+    "pages": 509,
+    "description": """
+        Eragon is the first book in The Inheritance Cycle
+        by American fantasy writer Christopher Paolini.
+    """}
+# Making the request passing json data
+requests.post("http://localhost:5000/api/v1/books", json=json.dumps(data))
+```
+
+### DELETE
+
+Use DELETE requests to remove books from the library.
+
+```python
+import requests
+
+book_id = 0
+requests.delete("http://localhost:5000/api/v1/books/%d" % book_id)
+```
+
+### PUT
+
+Use PUT requests to update specific books in the library.
+
+```python
+import json
+import requests
+
+book_id = 0
+data = {
+    "author": ["Christopher Paolini", "", ""],
+    "year": 2002}
+requests.put(
+    "http://localhost:5000/api/v1/books/%d" % book_id,
+    json=json.dumps(data))
+```
---
+++ book.py
@@ -0,0 +1,133 @@
+import json
+
+class Book:
+    DEFAULTS = {
+        "title": "",
+        "author": "",
+        "year": 0,
+        "genres": [""] * 3,
+        "pages": 0,
+        "description": ""
+    }
+    def __init__(self, id, title, author, year, genres, pages, description):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.year = year
+        self.genres = genres
+        self.pages = pages
+        self.description = description
+
+    @classmethod
+    def as_blank(cls):
+        return cls(*[None] * 4, [None] * 3, *[None] * 2)
+
+    @classmethod
+    def from_dict(cls, id, object, from_request=False):
+        if from_request:
+            genres = []
+            for i in range(len(
+                [k for k in object.keys() if k.startswith('genres')]
+            )):
+                genres.append(object["genres-%d" % i])
+        else:
+            genres = object["genres"]
+        return cls(
+            id,
+            object["title"],
+            object["author"],
+            int(object["year"]),
+            genres,
+            int(object["pages"]),
+            object["description"]
+        )
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "title": self.title,
+            "author": self.author,
+            "year": self.year,
+            "genres": self.genres,
+            "pages": self.pages,
+            "description": self.description
+        }
+
+class Library:
+    FILEPATH = ""
+    SORT_KEYS = {
+        "id": lambda x: x.id,
+        "title": lambda x: x.title,
+        "author": lambda x: x.author,
+        "year": lambda x: x.year,
+        "pages": lambda x: x.pages,
+        "asc": False,
+        "desc": True
+    }
+    def __init__(self, books=None):
+        if books:
+            self.books = books
+        else:
+            self.books = []
+
+    def add_book(self, book):
+        self.books.append(book)
+
+    def remove_book(self, id):
+        for book in self.books:
+            if book.id == id:
+                self.books.remove(book)
+                return True
+
+    def update_book(self, id, book):
+        self.remove_book(id)
+        self.add_book(book)
+
+    def get_book(self, id):
+        for book in self.books:
+            if book.id == id:
+                return book
+
+    def get_books(self, sort_key="id", sort_order="asc"):
+        key = self.SORT_KEYS[sort_key]
+        reverse = self.SORT_KEYS[sort_order]
+        return sorted(self.books, key=key, reverse=reverse)
+
+    def reorder(self):
+        for i, book in enumerate(self.get_books("id")):
+            book.id = i
+
+    @classmethod
+    def from_json(cls, filepath):
+        try:
+            with open(filepath) as obj:
+                data = json.load(obj)
+        except Exception as e:
+            print("Error - ", e)
+            return cls()
+
+        books = []
+        for item in data:
+            id = item.pop('id')
+            book = Book.from_dict(id, item)
+            books.append(book)
+        
+        return cls(books)
+
+    def to_json(self, books):
+        return [book.to_dict() for book in books]
+
+    @staticmethod
+    def str_to_json(data):
+        try:
+            return json.loads(data)
+        except:
+            return None
+
+    def save(self):
+        try:
+            with open(self.FILEPATH, "w") as obj:
+                books = self.to_json(self.books)
+                json.dump(books, obj)
+        except Exception as e:
+            print("Error - ", e)
